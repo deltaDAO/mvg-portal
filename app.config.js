d81810014cd3633f8e1810bd12b9548df99d2206
Moritz Kirstein <mkirstein@protonmail.com>
2021-11-11 01:08:50 +0100

    change networks

--- app.config.js
+++ app.config.js
@@ -10,10 +10,10 @@ module.exports = {
 
   // List of chainIds which metadata cache queries will return by default.
   // This preselects the Chains user preferences.
-  chainIds: [2021000, 2021001],
+  chainIds: [2021000],
 
   // List of all supported chainIds. Used to populate the Chains user preferences list.
-  chainIdsSupported: [2021000, 2021001],
+  chainIdsSupported: [2021000, 3, 4],
 
   rbacUrl: process.env.GATSBY_RBAC_URL,
 
