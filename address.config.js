b216559ecf377a4660e1127907b05c6b7e7289ec
Moritz Kirstein <mkirstein@protonmail.com>
2021-11-28 19:50:25 +0000

    refactor: remove whitelists

--- address.config.js
+++ address.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   whitelists: {
-    'publicKey.owner': [
-      '0xDF1c35c3d5178e9d98043b35a6737Bd861c191c9',
-      '0xa76Fa6837A6ffc9F123F2193717A5965c68B0cbA',
-      '0x4C84a36fCDb7Bc750294A7f3B5ad5CA8F74C4A52',
-      '0x5868f08084eFa4c7b075D6AC238229fF7fDa8669'
-    ],
-    dataToken: ['0xDd64fD4Ff847A2FBEC2596E7A58fbB439654acB5']
+    'publicKey.owner': [],
+    dataToken: []
   },
   featured: [
     'did:op:Dfa927A926481ce8Da68A9909785AD8d829804F2',
