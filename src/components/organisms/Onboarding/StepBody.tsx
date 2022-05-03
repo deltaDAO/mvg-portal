3328e6fe24eb5871b77aaf90bf8b7a85a1057be3
Luca Milanese <luca.milanese90@gmail.com>
2022-05-03 12:48:10 +0200

    fix: add refresh button to claim step in onboarding flow

--- src/components/organisms/Onboarding/StepBody.tsx
+++ src/components/organisms/Onboarding/StepBody.tsx
@@ -2,20 +2,35 @@ import React, { ReactElement } from 'react'
 import styles from './StepBody.module.css'
 import Markdown from '../../atoms/Markdown'
 import StepAction, { IStepAction } from './StepAction'
+import Button from '../../atoms/Button'
+import { ReactComponent as Refresh } from '../../../images/refresh.svg'
 
 export default function StepBody({
   body,
   image,
-  actions
+  actions,
+  refreshOption
 }: {
   body: string
   image?: string
   actions?: IStepAction[]
+  refreshOption?: boolean
 }): ReactElement {
   return (
     <div className={styles.content}>
       <div className={styles.cardContainer}>
         <div className={styles.card}>
+          {refreshOption && (
+            <div className={styles.refresh}>
+              <p>
+                Please, before you continue click on the button below to refresh
+                the page info.
+              </p>
+              <Button style="text" onClick={() => location.reload()}>
+                <Refresh /> Refresh
+              </Button>
+            </div>
+          )}
           <Markdown text={body} className={styles.paragraph} />
           <div className={styles.actions}>
             {actions?.map((action) => (
