70f769c95fdb4a62e61ed2e16acee030e148d00a
Luca Milanese <luca.milanese90@gmail.com>
2022-08-21 10:55:13 +0200

    feat: update edge teaser badge

--- src/components/atoms/EdgeAssetDetails.tsx
+++ src/components/atoms/EdgeAssetDetails.tsx
@@ -1,8 +1,8 @@
 import React, { ReactElement } from 'react'
 import { EdgeDDO } from '../../@types/edge/DDO'
 import styles from './EdgeAssetDetails.module.css'
-import { ReactComponent as Online } from '../../images/online.svg'
 import classNames from 'classnames/bind'
+import Badge from './Badge'
 
 const cx = classNames.bind(styles)
 
@@ -16,25 +16,24 @@ export default function EdgeAssetDetails({
   const numberOfAvailableAssets =
     service?.attributes?.main?.availableServices?.length
 
-  const iconStyles = cx({
-    icon: true,
+  const badgeStyles = cx({
+    badge: true,
     unavailable: !numberOfAvailableAssets
   })
   return (
     <div className={styles.container}>
       {assetModel && (
-        <div className={styles.detailsContainer}>
-          <Online
-            role="img"
-            aria-label="online assets"
-            className={iconStyles}
-          />
-          <div className={styles.details}>
+        <div className={styles.details}>
+          <div>
             <span className={styles.model}>{assetModel}</span>
-            <span className={styles.availableAssets}>
-              {`${numberOfAvailableAssets || 0} available assets`}
-            </span>
+            <Badge
+              label={numberOfAvailableAssets ? 'online' : 'offline'}
+              className={badgeStyles}
+            />
           </div>
+          <span className={styles.availableAssets}>
+            {`${numberOfAvailableAssets || 0} available assets`}
+          </span>
         </div>
       )}
     </div>
