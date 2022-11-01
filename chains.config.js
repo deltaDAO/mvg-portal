4d8d3f5fac7c2b6abfa052d4cc7a4a3586a5d7fb
Luca Milanese <luca.milanese90@gmail.com>
2022-11-01 17:24:22 +0100

    feat: update GEN-X contract addresses

--- chains.config.js
+++ chains.config.js
@@ -31,19 +31,19 @@ const chains = [
   },
   {
     chainId: GEN_X_NETWORK_ID,
-    dispenserAddress: '0xAa588d3737B611baFD7bD713445b314BD453a5C8',
+    dispenserAddress: '0x2a39940D98A4f9D01Ff0738c8420F360435b6A59',
     explorerUri: 'https://explorer.genx.minimal-gaia-x.eu/',
-    factoryAddress: '0x8f0483125FCb9aaAEFA9209D8E9d7b9C8B9Fb90F',
-    fixedRateExchangeAddress: '0x30753E4A8aad7F8597332E813735Def5dD395028',
+    factoryAddress: '0x325c09E2093C56AbDc86c5ccD68c77952e8034Af',
+    fixedRateExchangeAddress: '0x69Df9594E6A30a5751D170093059E7adb3Bf5e92',
     gasFeeMultiplier: 1,
     isDefault: true,
     metadataCacheUri: 'https://aquarius.delta-dao.com/',
-    metadataContractAddress: '0xFB88dE099e13c3ED21F80a7a1E49f8CAEcF10df6',
+    metadataContractAddress: '0xfA89407778041EF51B9e1aA16Ff85bDf908D17e6',
     name: 'GEN-X',
     network: 'genxtestnet',
     networkId: GEN_X_NETWORK_ID,
     nodeUri: 'http://194.182.169.98:8545/',
-    oceanTokenAddress: '0xa240c4d34b12dA624eCFE2e2adc88a18db93b02e',
+    oceanTokenAddress: '0x0995527d3473b3A98C471f1ED8787ACD77fBF009',
     oceanTokenSymbol: 'OCEAN',
     providerUri: 'https://provider.genx.delta-dao.com',
     startBlock: 1099,
@@ -51,7 +51,7 @@ const chains = [
     transactionBlockTimeout: 100,
     transactionConfirmationBlocks: 1,
     transactionPollingTimeout: 60000,
-    BFactory: '0x2C2B9C9a4a25e24B174f26114e8926a9f2128FE4'
+    BFactory: '0x7da756d49DFECb750B5ABa51c04088f257c3f8B4'
   }
 ]
 
