caea430e47b95ec791a50d1e70f8ed43a6f74f22
Luca Milanese <luca.milanese90@gmail.com>
2022-11-04 17:48:07 +0100

    feat: update featured asset list

--- address.config.js
+++ address.config.js
@@ -6,22 +6,33 @@ module.exports = {
   featured: [
     {
       title: 'Industry',
-      assets: []
+      assets: [
+        'did:op:3a0b09246457571009899C35B292057cA42DB773',
+        'did:op:106A1E02AB18F942708792043669fE831e7238BD'
+      ]
     },
     {
       title: 'Mobility',
       assets: [
         'did:op:a905d250B00b4CBd483225Bc3cF9E3cf8b02ac39',
-        'did:op:d51494c94d3aE025794034431f053e3fb6a26815'
+        'did:op:d51494c94d3aE025794034431f053e3fb6a26815',
+        'did:op:78f831361E1c850a16667346068f5fcAFE83F1F5',
+        'did:op:e69b8Ac90c25b099dC77b580B4d2019102fFCD3E'
       ]
     },
     {
       title: 'Science',
-      assets: []
+      assets: [
+        'did:op:1ea7A7F278669CfF1D0dD6CA608dd763fCcf8EE6',
+        'did:op:5A3E99a1d126F2546589bCF5f6cB541ab87Aad83'
+      ]
     },
     {
       title: 'Health, Finance, Business Analytics, Energy and more',
-      assets: []
+      assets: [
+        'did:op:048BdCeba7f43620302Cd7e593451e40f9e194F5',
+        'did:op:700b5C28396B4ceA3450A8DFA6297DF7c40C240f'
+      ]
     }
   ]
 }
