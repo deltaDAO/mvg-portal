e377ec027884240ab3649e12f878736cf7238389
Luca Milanese <luca.milanese90@gmail.com>
2022-12-08 00:13:41 +0100

    feat: add checksum check for dids in /asset

--- src/pages/asset/index.tsx
+++ src/pages/asset/index.tsx
@@ -4,13 +4,32 @@ import { PageProps } from 'gatsby'
 import PageTemplateAssetDetails from '../../components/templates/PageAssetDetails'
 import AssetProvider from '../../providers/Asset'
 import OceanProvider from '../../providers/Ocean'
+import Web3 from 'web3'
+import { useLocation, useNavigate } from '@reach/router'
 
 export default function PageGatsbyAssetDetails(props: PageProps): ReactElement {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const { origin } = location
+
   const [did, setDid] = useState<string>()
 
   useEffect(() => {
-    setDid(props.location.pathname.split('/')[2])
-  }, [props.location.pathname])
+    const did = props.location.pathname.split('/')[2]
+    const address = did.replace('did:op:', '0x')
+    const isAddress = Web3.utils.isAddress(address.toLowerCase())
+    const isChecksumValid = Web3.utils.checkAddressChecksum(address)
+
+    if (isAddress && !isChecksumValid) {
+      const checksumAddress = Web3.utils.toChecksumAddress(address)
+      const checksumDid = `did:op:${checksumAddress.replace('0x', '')}`
+      setDid(checksumDid)
+      navigate(`${origin}/asset/${checksumDid}`)
+      return
+    }
+
+    setDid(did)
+  }, [location, navigate, origin, props.location.pathname])
 
   return (
     <Permission eventType="browse">
