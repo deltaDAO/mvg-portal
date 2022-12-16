7993886f1c99cdbc6ed09c4d14048343e777b2ea
Moritz Kirstein <mkirstein@protonmail.com>
2022-12-16 13:26:56 +0100

    fix: remove empty link

--- src/components/pages/Profile/Header/VerifiedPatch.tsx
+++ src/components/pages/Profile/Header/VerifiedPatch.tsx
@@ -1,7 +1,5 @@
-import { Link } from 'gatsby'
 import React, { ReactElement } from 'react'
 import { ReactComponent as VerifiedPatchIcon } from '../../../../images/patch_check.svg'
-import { useProfile } from '../../../../providers/Profile'
 import Tooltip from '../../../atoms/Tooltip'
 import styles from './VerifiedPatch.module.css'
 
@@ -9,7 +7,6 @@ export default function VerifiedPatch(): ReactElement {
   const tooltip = (
     <span>
       This member has been officially onboarded to the GEN-X network.{' '}
-      <Link to="#">Learn more</Link>
     </span>
   )
 
