02ef9f23d06c5cd579c16c53297ec9c34f5878c0
Luca Milanese <luca.milanese90@gmail.com>
2023-01-11 10:17:19 +0100

    refactor: extract networks metadata config

--- scripts/write-networks-metadata.js
+++ scripts/write-networks-metadata.js
@@ -2,29 +2,20 @@
 'use strict'
 
 const axios = require('axios')
+const { networksMetadata } = require('../networksMetadata.config')
+const { GEN_X_NETWORK_ID } = require('../chains.config')
 
 // https://github.com/ethereum-lists/chains
 const chainDataUrl = 'https://chainid.network/chains.json'
 
 axios(chainDataUrl).then((response) => {
   // avoid having 2 nodes with the same chainId
-  const filteredData = response.data.filter((node) => node.chainId !== 100)
+  const filteredData = response.data.filter(
+    (node) => node.chainId !== GEN_X_NETWORK_ID
+  )
 
-  filteredData.push({
-    name: 'GEN-X Testnet',
-    chain: 'GEN-X',
-    network: 'testnet',
-    rpc: ['https://rpc.genx.minimal-gaia-x.eu'],
-    faucets: [],
-    nativeCurrency: {
-      name: 'GEN-X',
-      symbol: 'GX',
-      decimals: 18
-    },
-    infoURL: '',
-    shortName: 'GEN-X',
-    chainId: 100,
-    networkId: 100
-  })
-  process.stdout.write(JSON.stringify(filteredData, null, '  '))
+  // add custom networks metadata to the list
+  const fullNetworksMetadata = filteredData.concat(networksMetadata)
+
+  process.stdout.write(JSON.stringify(fullNetworksMetadata, null, '  '))
 })
