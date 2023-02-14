d06dba283ea191cc5a8e2e0099c470a7b50f24c9
Luca Milanese <luca.milanese90@gmail.com>
2023-02-14 14:38:21 +0100

    feat: add brand logo

--- src/components/@shared/atoms/Logo/index.tsx
+++ src/components/@shared/atoms/Logo/index.tsx
@@ -1,14 +1,21 @@
 import React, { ReactElement } from 'react'
 import LogoAssetFull from '@oceanprotocol/art/logo/logo.svg'
+import LogoAssetBranding from '@images/brand-logo.svg'
 import LogoAsset from '@images/logo.svg'
 import styles from './index.module.css'
 
 export interface LogoProps {
   noWordmark?: boolean
+  branding?: boolean
 }
 
-export default function Logo({ noWordmark }: LogoProps): ReactElement {
-  return noWordmark ? (
+export default function Logo({
+  noWordmark,
+  branding
+}: LogoProps): ReactElement {
+  return branding ? (
+    <LogoAssetBranding className={`${styles.logo} ${styles.branding}`} />
+  ) : noWordmark ? (
     <LogoAsset className={styles.logo} />
   ) : (
     <LogoAssetFull className={styles.logo} />
