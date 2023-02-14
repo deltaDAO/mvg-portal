0ecd0dff0035b160f4f9b3df712b17f89b522acd
Luca Milanese <luca.milanese90@gmail.com>
2023-02-14 09:31:15 +0100

    refactor: remove unused home sections

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,20 +4,13 @@ import Bookmarks from './Bookmarks'
 import { generateBaseQuery } from '@utils/aquarius'
 import { useUserPreferences } from '@context/UserPreferences'
 import { SortTermOptions } from '../../@types/aquarius/SearchQuery'
-import TopSales from './TopSales'
-import TopTags from './TopTags'
 import SectionQueryResult from './SectionQueryResult'
 import styles from './index.module.css'
-import Allocations from './Allocations'
-import MostViews from './MostViews'
 
 export default function HomePage(): ReactElement {
   const { chainIds } = useUserPreferences()
 
   const [queryLatest, setQueryLatest] = useState<SearchQuery>()
-  const [queryMostSales, setQueryMostSales] = useState<SearchQuery>()
-
-  const [queryMostAllocation, setQueryMostAllocation] = useState<SearchQuery>()
 
   useEffect(() => {
     const baseParams = {
@@ -30,27 +23,6 @@ export default function HomePage(): ReactElement {
       } as SortOptions
     } as BaseQueryParams
     setQueryLatest(generateBaseQuery(baseParams))
-
-    const baseParamsSales = {
-      chainIds,
-      esPaginationOptions: {
-        size: 6
-      },
-      sortOptions: {
-        sortBy: SortTermOptions.Orders
-      } as SortOptions
-    } as BaseQueryParams
-    setQueryMostSales(generateBaseQuery(baseParamsSales))
-    const baseParamsAllocation = {
-      chainIds,
-      esPaginationOptions: {
-        size: 6
-      },
-      sortOptions: {
-        sortBy: SortTermOptions.Allocated
-      } as SortOptions
-    } as BaseQueryParams
-    setQueryMostAllocation(generateBaseQuery(baseParamsAllocation))
   }, [chainIds])
 
   return (
@@ -59,19 +31,6 @@ export default function HomePage(): ReactElement {
         <h3>Your Bookmarks</h3>
         <Bookmarks />
       </section>
-
-      <Allocations />
-
-      <SectionQueryResult
-        title="Highest veOCEAN Allocations"
-        query={queryMostAllocation}
-      />
-
-      <SectionQueryResult title="Most Sales" query={queryMostSales} />
-      <MostViews />
-      <TopSales title="Publishers With Most Sales" />
-      <TopTags title="Top Tags By Sales" />
-
       <SectionQueryResult
         title="Recently Published"
         query={queryLatest}
