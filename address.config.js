46832c17cbcedb5e504fb2a6c4438b265e1bc565
Kai Meinke <59994024+kaimeinke@users.noreply.github.com>
2023-05-28 20:15:29 +0200

    Update address.config.js
    New Frontpage Assets with moveID

--- address.config.js
+++ address.config.js
@@ -5,6 +5,22 @@ module.exports = {
   },
   featured: [
     {
+      title: 'Smart Mobility, Automotive',
+      assets: [
+        'did:op:b4be1f60c197890bfebc041e66731a2c6f98f520169a9f38173c19e29353b4d0', // moveID Wolfsburg Smart Garage EV Charging Status Data for Compute
+        'did:op:684af1448fd5c99d5e4839da4f6e206056be74defd206a29307cc68ea464557f', // Smart EV Garage Status Aggregation
+        'did:op:99fda160c29d15c4c9d2216033da9594f9932176229f77cfadf53dbf12cc4dc8', // moveID Wolfsburg Smart Parking and Charging Garage Status
+        'did:op:b4be1f60c197890bfebc041e66731a2c6f98f520169a9f38173c19e29353b4d0', // Road Damage Detection (FREE)
+        'did:op:2e148b5c1ff12590c13bc018100743f0253f22f49cbaf7876612b2417f961789', // 01 - Vehicle Data Collection (EUR)
+        'did:op:52a1c78e5569e09514e293602cd38c10b7d011758ce269c7f07b6ddf46f2c8cb', // SAVe Marktkauf Kreuzung
+        'did:op:025422379b72f683deb0facd08be6f87b845641116bf7f9f2cfbb0bc9efc82b6', // CARISSMA Indoor Track (Road5)
+        'did:op:8e3f2acad2b9d856038f23c86190c7052ba9f7472675e302bd44867d6bb974e0', // Lidar Sensor Model
+        'did:op:87b0133e6979419919d8526e3bac2604bd8b04d0fca43e21fb3fde67497a7860', // Traffic Object Annotation for Highway Image Data
+        'did:op:f3e162d564b5a925edaf2ce9d8cb3f52347f63376f337b4c6b910d22d93f02bc', // Morning Rush Intensities / INWEVA Spitsen 2020 (RWS)
+        'did:op:8c2cc9d84e76c14fdf3c833f5dd8fbeec7748ec7ec21511434fc52f30153101a' // Traffic Data for Object Annotation (RWS)
+      ]
+    },
+    {
       title: 'Manufacturing, Industry 4.0',
       assets: [
         'did:op:dd456754dc659ef111cc5fa87662cda1a67ca390c3f86fee4d5592ecb3bbffd5', // Supplier No. 1 - Component Data for Part Matching
@@ -19,20 +35,6 @@ module.exports = {
       ]
     },
     {
-      title: 'Mobility',
-      assets: [
-        'did:op:b4be1f60c197890bfebc041e66731a2c6f98f520169a9f38173c19e29353b4d0', // Road Damage Detection (FREE)
-        'did:op:2e148b5c1ff12590c13bc018100743f0253f22f49cbaf7876612b2417f961789', // 01 - Vehicle Data Collection (EUR)
-        'did:op:817223107808947f1c6b2877651d496557bccc4b6385b35e0bbeb01a9ed91b04', // 07 - Vehicle Data Collection (OCEAN)
-        'did:op:52a1c78e5569e09514e293602cd38c10b7d011758ce269c7f07b6ddf46f2c8cb', // SAVe Marktkauf Kreuzung
-        'did:op:025422379b72f683deb0facd08be6f87b845641116bf7f9f2cfbb0bc9efc82b6', // CARISSMA Indoor Track (Road5)
-        'did:op:8e3f2acad2b9d856038f23c86190c7052ba9f7472675e302bd44867d6bb974e0', // Lidar Sensor Model
-        'did:op:87b0133e6979419919d8526e3bac2604bd8b04d0fca43e21fb3fde67497a7860', // Traffic Object Annotation for Highway Image Data
-        'did:op:f3e162d564b5a925edaf2ce9d8cb3f52347f63376f337b4c6b910d22d93f02bc', // Morning Rush Intensities / INWEVA Spitsen 2020 (RWS)
-        'did:op:8c2cc9d84e76c14fdf3c833f5dd8fbeec7748ec7ec21511434fc52f30153101a', // Traffic Data for Object Annotation (RWS)
-      ]
-    },
-    {
       title: 'Text Analysis, Language Processing, and more',
       assets: [
         'did:op:13731acde4143a7dc1c0dcfbd999b7574ea5f1ee75ba5bb77e2c1485601d2b4a', // CrossAsia Probability Calculation Algorithm for Topics in Time
@@ -41,6 +43,9 @@ module.exports = {
         'did:op:bff53f8c36f2156cff80e5fa18f39d76cb3c52617694a7cde4635826209c259c', // CrossAsia Text Analysis Algorithm
         'did:op:9308b56b84b2475ff8e9a8251adba96dbd9461bd528ef4b0c98638d11ab5375b', // BIMKIT Text Localization in Architectural Floor Plans
         'did:op:1c77a8c28444f5405d8a23ad88d5579c12be574712378ca5b5a6385730412e1c'  // BIMKIT Algorithm for Text Localization in Architectural Floor Plans
+        'did:op:f231ad11244f2c2e86d694a24765d49dccdaddf1b48b4b0b74ebd586a44bac39', // Die Handelswege und Verkehrsmittel der Gegenwart
+        'did:op:b6e20571d7b403895aed65e0d5156e4a1dad809c969c38c6fc2154bb26c33d12', // Stanley's sämtliche Reisen in Afrika
+        'did:op:2add32a20d0ee146c51424907c1a4397f50d985cb88f0738c79cc5602e2b70a7' // Eines deutschen Matrosen Nordpolarfahrten
       ]
     },
     {
