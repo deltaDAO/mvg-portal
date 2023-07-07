ce423b5e3df58266954cfe504ab578af9df0f5f3
Luca Milanese <luca.milanese90@gmail.com>
2023-07-07 10:08:14 +0200

    feat: extend ComputeEnvironment type to include fee token

--- src/@types/Compute.d.ts
+++ src/@types/Compute.d.ts
@@ -1,4 +1,4 @@
-import { ComputeJob } from '@oceanprotocol/lib'
+import { ComputeEnvironment, ComputeJob } from '@oceanprotocol/lib'
 import { OrdersData_orders_datatoken as OrdersDatatoken } from '../@types/subgraph/OrdersData'
 
 // declaring into global scope to be able to use this as
@@ -31,5 +31,9 @@ declare global {
   interface totalPriceMap {
     value: string
     symbol: string
+  }
+
+  interface ComputeEnvironmentExtended extends ComputeEnvironment {
+    feeToken: string
   }
 }
