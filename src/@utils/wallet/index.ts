e7f4e26853047ee07f9192f273b085307c2a3d40
Moritz Kirstein <mkirstein@protonmail.com>
2023-09-12 18:34:01 +0200

    feat: add getAlowance utils function

--- src/@utils/wallet/index.ts
+++ src/@utils/wallet/index.ts
@@ -151,6 +151,14 @@ export async function addCustomNetwork(
   )
 }
 
+export function getAdjustDecimalsValue(
+  value: number,
+  decimals: number
+): string {
+  const adjustedDecimalsValue = `${value}${'0'.repeat(18 - decimals)}`
+  return formatEther(adjustedDecimalsValue)
+}
+
 export async function getTokenBalance(
   accountId: string,
   decimals: number,
@@ -162,10 +170,29 @@ export async function getTokenBalance(
   try {
     const token = new Contract(tokenAddress, erc20ABI, web3Provider)
     const balance = await token.balanceOf(accountId)
-    const adjustedDecimalsBalance = `${balance}${'0'.repeat(18 - decimals)}`
-    return formatEther(adjustedDecimalsBalance)
+
+    return getAdjustDecimalsValue(balance, decimals)
   } catch (e) {
     LoggerInstance.error(`ERROR: Failed to get the balance: ${e.message}`)
+  }
+}
+
+export async function getTokenAllowance(
+  owner: string,
+  spender: string,
+  decimals: number,
+  tokenAddress: string,
+  web3Provider: ethers.providers.Provider
+): Promise<string> {
+  if (!web3Provider || !owner || !spender || !tokenAddress) return
+
+  try {
+    const token = new Contract(tokenAddress, erc20ABI, web3Provider)
+    const allowance = await token.allowance(owner, spender)
+
+    return getAdjustDecimalsValue(allowance, decimals)
+  } catch (e) {
+    LoggerInstance.error(`ERROR: Failed to get the allowance: ${e.message}`)
   }
 }
 
