a7f0897113dd4c2cc19eec4aa55f6a8c8a116b0b
Luca Milanese <luca.milanese90@gmail.com>
2023-09-26 17:02:30 +0200

    feat: add accordion component

---
+++ src/components/@shared/Accordion/index.tsx
@@ -0,0 +1,48 @@
+import React, { ReactElement, ReactNode, useState } from 'react'
+import Button from '@shared/atoms/Button'
+import styles from './index.module.css'
+import Caret from '@images/caret.svg'
+import classNames from 'classnames/bind'
+import Badge from '@shared/atoms/Badge'
+
+const cx = classNames.bind(styles)
+
+export default function Accordion({
+  title,
+  defaultState = false,
+  badgeNumber,
+  children
+}: {
+  title: string
+  defaultState?: boolean
+  badgeNumber?: number
+  children: ReactNode
+}): ReactElement {
+  const [open, setOpen] = useState(defaultState)
+
+  async function handleClick() {
+    setOpen(!open)
+  }
+
+  return (
+    <div className={cx({ actions: true, open })}>
+      <h3 className={styles.title} onClick={handleClick}>
+        <span>
+          {title}
+          {badgeNumber > 0 && (
+            <Badge label={badgeNumber} className={styles.badge} />
+          )}
+        </span>
+        <Button
+          style="text"
+          size="small"
+          onClick={handleClick}
+          className={styles.toggle}
+        >
+          <Caret />
+        </Button>
+      </h3>
+      <div className={styles.content}>{children}</div>
+    </div>
+  )
+}
