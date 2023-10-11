0065aea4f02b495f3a3f056c67fedd5c7af8782f
Luca Milanese <luca.milanese90@gmail.com>
2023-10-11 17:44:54 +0200

    chore: remove categories filter type

--- filters.config.js
+++ filters.config.js
@@ -1,46 +1,4 @@
 module.exports = {
-  filters: [
-    {
-      id: 'filterSet',
-      label: 'Categories',
-      type: 'filterList',
-      options: [
-        {
-          label: 'automotive',
-          value: 'automotive'
-        },
-        {
-          label: 'manufacturing',
-          value: 'manufacturing'
-        },
-        {
-          label: 'text analysis',
-          value: 'textAnalysis'
-        },
-        {
-          label: 'finance',
-          value: 'finance'
-        }
-      ]
-    }
-  ],
-  filterSets: {
-    automotive: [
-      'charging',
-      'ev',
-      'gx4m',
-      'mobility',
-      'moveid',
-      'parking',
-      'traffic'
-    ],
-    manufacturing: [
-      'euprogigant',
-      'industry40',
-      'manufacturing',
-      'predictive-maintenance'
-    ],
-    textAnalysis: ['library', 'ocr', 'text-analysis'],
-    finance: ['graphql']
-  }
+  filters: [],
+  filterSets: {}
 }
