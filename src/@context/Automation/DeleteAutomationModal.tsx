1b8d8f369b95d8761aff1f3046252f7ed3bd6e99
Moritz Kirstein <mkirstein@protonmail.com>
2023-10-16 14:22:20 +0200

    chore: remove unused imports

--- src/@context/Automation/DeleteAutomationModal.tsx
+++ src/@context/Automation/DeleteAutomationModal.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect, useState } from 'react'
+import React, { ReactElement } from 'react'
 import Modal from '../../components/@shared/atoms/Modal'
 import styles from './DeleteAutomationModal.module.css'
 import Button from '../../components/@shared/atoms/Button'
