ef2378238ae1fad885f3a16209251690686e4d93
Albert Peci <albert.peci@posteo.de>
2024-08-06 22:51:18 +0200

    refactor: rename function to PageFaucet()

--- src/pages/faucet.tsx
+++ src/pages/faucet.tsx
@@ -3,7 +3,7 @@ import Faucet from '../components/Faucet'
 import content from '../../content/pages/faucet.json'
 import Page from '@components/@shared/Page'
 
-export default function PageVerify(): ReactElement {
+export default function PageFaucet(): ReactElement {
   return (
     <Page title={content.title} description={content.description} uri="">
       <Faucet />
