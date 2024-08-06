d4b366c0ee1dca5ea0fcbcc593feb24c0912d030
Albert Peci <albert.peci@posteo.de>
2024-08-06 22:21:52 +0200

    refactor(faucet): rename Verify to Faucet

--- src/pages/faucet.tsx
+++ src/pages/faucet.tsx
@@ -1,12 +1,12 @@
 import { ReactElement } from 'react'
-import Verify from '../components/Faucet'
+import Faucet from '../components/Faucet'
 import content from '../../content/pages/faucet.json'
 import Page from '@components/@shared/Page'
 
 export default function PageVerify(): ReactElement {
   return (
     <Page title={content.title} description={content.description} uri="">
-      <Verify />
+      <Faucet />
     </Page>
   )
 }
