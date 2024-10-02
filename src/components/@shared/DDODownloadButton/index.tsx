0ed7eb3f160396a98c70b7a943e58c0e750322b9
KevinRohlf <123676073+KevinRohlf@users.noreply.github.com>
2024-10-02 20:10:14 +0200

    refactor: extract downloadJSON function, add JSON validation, and make download DDO button visible outside debug mode

--- src/components/@shared/DDODownloadButton/index.tsx
+++ src/components/@shared/DDODownloadButton/index.tsx
@@ -1,22 +1,18 @@
 import { ReactElement } from 'react'
 import Button from '../atoms/Button'
+import { downloadJSON } from '@utils/downloadJSON'
 
 export default function DDODownloadButton({
   asset
 }: {
   asset: any
 }): ReactElement {
-  // Download DDO as JSON
-  const downloadJson = () => {
-    const element = document.createElement('a') // Create a tag
-    const file = new Blob([JSON.stringify(asset)], {
-      type: 'application/json'
-    }) // Create a file
-    element.href = URL.createObjectURL(file) // Create a URL
-    element.download = `${asset.metadata.name}_metadata.json` // Set the file name
-    document.body.appendChild(element) // Append the tag to the body
-    element.click() // Click the a element
-  }
-
-  return <Button onClick={downloadJson}>Download DDO</Button>
+  return (
+    <Button
+      onClick={() => downloadJSON(asset, `${asset.metadata.name}_metadata`)}
+      size="small"
+    >
+      Download DDO
+    </Button>
+  )
 }
--- src/components/@shared/DebugOutput/index.tsx
+++ src/components/@shared/DebugOutput/index.tsx
@@ -1,5 +1,4 @@
 import { ReactElement } from 'react'
-import DDODownloadButton from '../DDODownloadButton'
 
 export default function DebugOutput({
   title,
@@ -10,16 +9,7 @@ export default function DebugOutput({
 }): ReactElement {
   return (
     <div style={{ marginTop: 'var(--spacer)' }}>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          flexWrap: 'wrap'
-        }}
-      >
-        {title && <h5>{title}</h5>}
-        {output && <DDODownloadButton asset={output} />}
-      </div>
+      {title && <h5>{title}</h5>}
       <pre style={{ wordWrap: 'break-word' }}>
         <code>{JSON.stringify(output, null, 2)}</code>
       </pre>
