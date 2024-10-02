4e9f46bf3cd9b091a6e61305bbbbefb9b8f0adb5
KevinRohlf <123676073+KevinRohlf@users.noreply.github.com>
2024-10-02 13:36:20 +0200

    feat: added comments

--- src/components/@shared/DDODownloadButton/index.tsx
+++ src/components/@shared/DDODownloadButton/index.tsx
@@ -6,15 +6,16 @@ export default function DDODownloadButton({
 }: {
   asset: any
 }): ReactElement {
+  // Download DDO as JSON
   const downloadJson = () => {
-    const element = document.createElement('a')
+    const element = document.createElement('a') // Create a tag
     const file = new Blob([JSON.stringify(asset)], {
       type: 'application/json'
-    })
-    element.href = URL.createObjectURL(file)
-    element.download = `${asset.metadata.name}_metadata.json`
-    document.body.appendChild(element)
-    element.click()
+    }) // Create a file
+    element.href = URL.createObjectURL(file) // Create a URL
+    element.download = `${asset.metadata.name}_metadata.json` // Set the file name
+    document.body.appendChild(element) // Append the tag to the body
+    element.click() // Click the a element
   }
 
   return <Button onClick={downloadJson}>Download DDO</Button>
