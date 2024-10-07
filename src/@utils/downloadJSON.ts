1778318490c2077d66fed4797a05a3dbbe9ee6e8
Kevin Rohlf <123676073+KevinRohlf@users.noreply.github.com>
2024-10-07 15:12:08 +0200

    Feat/ddo export (#593)
    * feat: add download DDO button
    
    * feat: added comments
    
    * refactor: extract downloadJSON function, add JSON validation, and make download DDO button visible outside debug mode
    
    * refactor: update JSON validation

---
+++ src/@utils/downloadJSON.ts
@@ -0,0 +1,23 @@
+export function isValidJSON(json: string): boolean {
+  try {
+    JSON.parse(json)
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+// Download JSON file
+export function downloadJSON(json: string, filename: string): void {
+  // check if the json is valid
+  if (isValidJSON(json)) {
+    const element = document.createElement('a') // create an <a> tag
+    const file = new Blob([json], {
+      type: 'application/json'
+    }) // create a file
+    element.href = URL.createObjectURL(file) // create a URL for the file
+    element.download = `${filename}.json` // Set the file name
+    document.body.appendChild(element) // Append the tag to the body
+    element.click() // Click the a element
+  }
+}
---
+++ src/components/@shared/DDODownloadButton/index.tsx
@@ -0,0 +1,20 @@
+import { ReactElement } from 'react'
+import Button from '../atoms/Button'
+import { downloadJSON } from '@utils/downloadJSON'
+
+export default function DDODownloadButton({
+  asset
+}: {
+  asset: any
+}): ReactElement {
+  return (
+    <Button
+      onClick={() =>
+        downloadJSON(JSON.stringify(asset), `${asset.metadata.name}_metadata`)
+      }
+      size="small"
+    >
+      Download DDO
+    </Button>
+  )
+}
