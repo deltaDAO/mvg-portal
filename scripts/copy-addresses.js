ca5040982400d69f13321bd6aea5d4b1d00748eb
Kevin Rohlf <123676073+KevinRohlf@users.noreply.github.com>
2024-10-25 12:17:13 +0200

    chore: remove gen-x from all files (#612)

--- scripts/copy-addresses.js
+++ scripts/copy-addresses.js
@@ -5,17 +5,13 @@ const addressJsonFile = join(
   __dirname,
   '../node_modules/@oceanprotocol/contracts/addresses/address.json'
 )
-const genxAddressesFile = join(__dirname, '../genxAddress.json')
 
 const addressJson = JSON.parse(readFileSync(addressJsonFile).toString())
 
-const genxAddresses = JSON.parse(readFileSync(genxAddressesFile).toString())
-
 const newObject = {
-  ...addressJson,
-  ...genxAddresses
+  ...addressJson
 }
 
 writeFileSync(addressJsonFile, JSON.stringify(newObject))
 
-console.log('Added GEN-X addresses to', addressJsonFile)
+console.log('Added addresses to', addressJsonFile)
