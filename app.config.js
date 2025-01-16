2495225c6a96d820a8ccc40b7977b8ecdeb7f098
Filip Masar <filip.masar99@gmail.com>
2025-01-16 12:43:40 +0100

    add optimism sepolia

--- app.config.js
+++ app.config.js
@@ -11,10 +11,10 @@ module.exports = {
 
   // List of chainIds which metadata cache queries will return by default.
   // This preselects the Chains user preferences.
-  chainIds: [11155111, 8996],
+  chainIds: [11155111, 8996, 11155420],
 
   // List of all supported chainIds. Used to populate the Chains user preferences list.
-  chainIdsSupported: [11155111, 8996],
+  chainIdsSupported: [11155111, 8996, 11155420],
 
   customProviderUrl: process.env.NEXT_PUBLIC_PROVIDER_URL,
 
