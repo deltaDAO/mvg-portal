e7aa7dc14393622f87bd1aa0b865f104154d6bbf
Filip Masar <filip.masar99@gmail.com>
2025-01-29 16:15:15 +0100

    support custom rpc for sepolia

--- src/@utils/ocean/index.ts
+++ src/@utils/ocean/index.ts
@@ -40,6 +40,11 @@ export function getOceanConfig(network: string | number): Config {
     config = { ...config, ...sanitizeDevelopmentConfig(config) }
   }
 
+  // Override RPC URL for Sepolia if it's set (the reason is ocean.js supports only infura)
+  if (network === 11155111 && process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL) {
+    config.nodeUri = process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL
+  }
+
   return config as Config
 }
 
