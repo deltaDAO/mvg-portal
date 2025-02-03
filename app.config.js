29695f67d8e5dd1e6de0e2d6c5ab9e0418d80c8d
Kevin Rohlf <123676073+KevinRohlf@users.noreply.github.com>
2025-02-03 12:11:55 +0100

    Feat/default terms and conditions variable (#653)
    * feat: rename defaultAccessTerms to defaultTermsAndConditionsUrl
    
    * fix: update terms and conditions link
    
    * refactor: integrate reusable Terms and Conditions checkbox component
    
    * feat: add Terms and Conditions checkboxes for download
    
    * feat: update Terms and Conditions structure
    
    * feat: update default terms and conditions URL

--- app.config.js
+++ app.config.js
@@ -77,8 +77,7 @@ module.exports = {
     process.env.NEXT_PUBLIC_PRIVACY_PREFERENCE_CENTER || 'true',
 
   // Default terms to be used for service offerings made on this marketplace
-  defaultAccessTerms:
-    'https://raw.githubusercontent.com/deltaDAO/mvg-portal/v4/content/pages/terms.md',
+  defaultTermsAndConditionsUrl: 'https://portal.pontus-x.eu/privacy/en',
 
   // Purgatory URI, leave as an empty string to disable the API call
   purgatoryUrl: process.env.NEXT_PUBLIC_PURGATORY_URI || '',
