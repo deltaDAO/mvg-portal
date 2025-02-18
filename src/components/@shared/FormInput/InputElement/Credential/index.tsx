6d79231ca918e9d1c56e7006abda5dc81e9e2048
Kevin Rohlf <123676073+KevinRohlf@users.noreply.github.com>
2025-02-18 11:14:23 +0100

    feat: display verified address names in Credentials component (#664)
    * feat: display verified address names in Credentials component
    
    * feat: add getVerifiedAddressName function to useAddressConfig hook
    
    * fix: update getVerifiedAddressName function adding a return type and refactor the return
    
    * refactor: getVerifiedAddressName function to simplify syntax and add return type
    
    * refactor: simplify syntax of getVerifiedAddressName function

--- src/components/@shared/FormInput/InputElement/Credential/index.tsx
+++ src/components/@shared/FormInput/InputElement/Credential/index.tsx
@@ -7,11 +7,13 @@ import { toast } from 'react-toastify'
 import InputGroup from '../../InputGroup'
 import InputElement from '..'
 import { InputProps } from '../..'
+import { useAddressConfig } from '@hooks/useAddressConfig'
 
 export default function Credentials(props: InputProps) {
   const [field, meta, helpers] = useField(props.name)
   const [addressList, setAddressList] = useState<string[]>(field.value || [])
   const [value, setValue] = useState('')
+  const { getVerifiedAddressName } = useAddressConfig()
 
   useEffect(() => {
     helpers.setValue(addressList)
@@ -35,6 +37,11 @@ export default function Credentials(props: InputProps) {
     }
     setAddressList((addressList) => [...addressList, value.toLowerCase()])
     setValue('')
+  }
+
+  const showAddressName = (address: string) => {
+    const addressName = getVerifiedAddressName(address)
+    return addressName ? `${address} (${addressName})` : address
   }
 
   return (
@@ -68,7 +75,7 @@ export default function Credentials(props: InputProps) {
                   <InputElement
                     className={styles.address}
                     name={`address[${i}]`}
-                    value={value}
+                    value={showAddressName(value)}
                     disabled
                   />
                   <Button
