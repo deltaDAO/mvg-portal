2e80f55f1f10456b9f3339ceaaf5df8a093586c9
Moritz Kirstein <mkirstein@protonmail.com>
2025-03-19 19:43:53 +0100

    chore: add export output

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = (phase, { defaultConfig }) => {
    * @type {import('next').NextConfig}
    */
   const nextConfig = {
+    output: 'export',
     webpack: (config, options) => {
       config.module.rules.push(
         {
