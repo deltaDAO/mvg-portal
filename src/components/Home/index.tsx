ffc5fb123f329f27a58fd5eef3f22429fe24107c
ahmedraza118 <ahmedrazachrtx@gmail.com>
2025-04-10 13:18:11 +0300

    handle search and publish redirection on HomePage

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,17 @@
-import React, { ReactElement } from 'react'
+import {
+  useState,
+  useEffect,
+  ChangeEvent,
+  FormEvent,
+  KeyboardEvent,
+  ReactElement,
+  useRef
+} from 'react'
+import type { MouseEvent } from 'react'
 import Image from 'next/image'
 import { useMarketMetadata } from '@context/MarketMetadata'
 import styles from './index.module.css'
+import InputElement from '@shared/FormInput/InputElement'
 
 import Features from './Features/Features'
 import Upload from '@images/publish.svg'
@@ -12,8 +22,88 @@ import EuproLogo from '@images/eupro.svg'
 import OceanHeroLogo from '@images/ocean-hero.svg'
 import SearchLogo from '@images/search.svg'
 import Menu from './Menu/Menu'
+import { addExistingParamsToUrl } from '../Search/utils'
+import { useRouter } from 'next/router'
+import { useSearchBarStatus } from '@context/SearchBarStatus'
 
-function HeroSection(): ReactElement {
+async function emptySearch() {
+  const searchParams = new URLSearchParams(window?.location.href)
+  const text = searchParams.get('text')
+
+  if (text !== '' && text !== undefined && text !== null) {
+    await addExistingParamsToUrl(location, ['text', 'owner', 'tags'])
+  }
+}
+
+function HeroSection({
+  placeholder,
+  initialValue,
+  isSearchPage
+}: {
+  placeholder?: string
+  initialValue?: string
+  isSearchPage?: boolean
+}): ReactElement {
+  const router = useRouter()
+  const [value, setValue] = useState(initialValue || '')
+  const parsed = router.query
+  const searchBarRef = useRef<HTMLInputElement>(null)
+  const {
+    isSearchBarVisible,
+    setSearchBarVisible,
+    homeSearchBarFocus,
+    setHomeSearchBarFocus
+  } = useSearchBarStatus()
+
+  useEffect(() => {
+    if (parsed?.text || parsed?.owner)
+      setValue((parsed?.text || parsed?.owner) as string)
+  }, [parsed?.text, parsed?.owner])
+
+  useEffect(() => {
+    setSearchBarVisible(false)
+    setHomeSearchBarFocus(false)
+  }, [setSearchBarVisible, setHomeSearchBarFocus])
+
+  useEffect(() => {
+    if (!isSearchBarVisible && !homeSearchBarFocus) return
+    if (searchBarRef?.current) {
+      searchBarRef.current.focus()
+    }
+  }, [isSearchBarVisible, homeSearchBarFocus])
+
+  async function startSearch() {
+    if (value === '') setValue(' ')
+
+    const urlEncodedValue = encodeURIComponent(value)
+    const url = await addExistingParamsToUrl(location, [
+      'text',
+      'owner',
+      'tags'
+    ])
+    router.push(`${url}&text=${urlEncodedValue}`)
+  }
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setValue(e.target.value)
+    e.target.value === '' && emptySearch()
+  }
+
+  async function handleKeyPress(e: KeyboardEvent<HTMLInputElement>) {
+    if (e.key === 'Enter') {
+      await startSearch()
+    }
+  }
+
+  async function handleButtonClick(e: FormEvent<HTMLButtonElement>) {
+    e.preventDefault()
+    await startSearch()
+  }
+  const handlePublishClick = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    router.push('/publish/1')
+  }
+
   return (
     <section className={styles.hero}>
       <div className={styles.contentContainer}>
@@ -33,7 +123,7 @@ function HeroSection(): ReactElement {
           <div className={styles.ctaContainer}>
             <div className={styles.ctaBlock}>
               <h3 className={styles.ctaTitle}>Publish an asset</h3>
-              <button className={styles.ctaButton}>
+              <button className={styles.ctaButton} onClick={handlePublishClick}>
                 <div className={styles.buttonContent}>
                   <Upload className={styles.uploadIcon} />
                   <span className={styles.buttonText}>Publish</span>
@@ -43,19 +133,32 @@ function HeroSection(): ReactElement {
 
             <div className={styles.divider}></div>
 
-            <div className={styles.searchBlock}>
+            <form
+              className={styles.searchBlock}
+              autoComplete={!value ? 'off' : 'on'}
+            >
               <h3 className={styles.ctaTitle}>Search for data</h3>
               <div className={styles.searchContainer}>
-                <input
-                  type="text"
-                  placeholder="Search"
+                <InputElement
+                  ref={searchBarRef}
+                  type="search"
+                  name="search"
+                  placeholder={placeholder || 'Search...'}
+                  value={value}
+                  onChange={handleChange}
+                  required
+                  size="small"
                   className={styles.searchInput}
+                  onKeyPress={handleKeyPress}
                 />
-                <button className={styles.searchButton}>
+                <button
+                  onClick={handleButtonClick}
+                  className={styles.searchButton}
+                >
                   <SearchLogo className={styles.searchIcon} />
                 </button>
               </div>
-            </div>
+            </form>
           </div>
         </div>
 
@@ -88,8 +191,6 @@ function HeroSection(): ReactElement {
 }
 
 export default function HomePage(): ReactElement {
-  const { siteContent } = useMarketMetadata()
-
   return (
     <>
       <HeroSection />
