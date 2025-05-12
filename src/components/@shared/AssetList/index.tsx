e9c1861a7cddfa05f0d4225fb5d88a92ca99be05
ahmedraza118 <ahmedrazachrtx@gmail.com>
2025-05-12 17:22:07 +0300

    pushed catalogue page changes

--- src/components/@shared/AssetList/index.tsx
+++ src/components/@shared/AssetList/index.tsx
@@ -116,12 +116,12 @@ export default function AssetList({
     <Loader />
   ) : (
     <>
-      {showAssetViewSelector && (
+      {/* {showAssetViewSelector && (
         <AssetViewSelector
           activeAssetView={activeAssetView}
           setActiveAssetView={setActiveAssetView}
         />
-      )}
+      )} */}
       <div className={styleClasses}>
         {assets?.length > 0 && assets[0] !== undefined ? (
           <>
