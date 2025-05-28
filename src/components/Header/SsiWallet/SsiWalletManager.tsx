976c238c1f882c36fca9c9a4b16b59de41335e20
AdriGeorge <adrian7ko@gmail.com>
2025-05-28 15:41:43 +0300

    fix: fix

--- src/components/Header/SsiWallet/SsiWalletManager.tsx
+++ src/components/Header/SsiWallet/SsiWalletManager.tsx
@@ -4,12 +4,15 @@ import { useSsiWallet } from '@context/SsiWallet'
 import { useUserPreferences } from '@context/UserPreferences'
 import {
   connectToWallet,
+  getWalletKeys,
+  getWallets,
   setSsiWalletApiOverride,
   STORAGE_KEY
 } from '@utils/wallet/ssiWallet'
 import appConfig from 'app.config.cjs'
 import { LoggerInstance } from '@oceanprotocol/lib'
 import SsiApiModal from '../Wallet/SsiApiModal'
+import { SsiWalletDesc, SsiWalletSession } from 'src/@types/SsiWallet'
 
 export default function SsiWalletManager() {
   const { showSsiWalletModule, setShowSsiWalletModule } = useUserPreferences()
@@ -18,12 +21,43 @@ export default function SsiWalletManager() {
     setSessionToken,
     ssiWalletCache,
     setCachedCredentials,
-    clearVerifierSessionCache
+    clearVerifierSessionCache,
+    sessionToken,
+    selectedWallet,
+    setSelectedWallet,
+    setSelectedKey
   } = useSsiWallet()
 
   const [overrideApi, setOverrideApi] = useState(() => {
     return sessionStorage.getItem(STORAGE_KEY) || appConfig.ssiWalletApi
   })
+
+  const fetchWallets = async (session: SsiWalletSession) => {
+    try {
+      if (sessionToken) {
+        const wallets = await getWallets(session.token)
+        setSelectedWallet(wallets[0])
+        return wallets[0]
+      }
+    } catch (error) {
+      return selectedWallet
+    }
+  }
+
+  const fetchKeys = async (
+    wallet: SsiWalletDesc,
+    session: SsiWalletSession
+  ) => {
+    if (!selectedWallet || !sessionToken) {
+      return
+    }
+    try {
+      const keys = await getWalletKeys(wallet, session.token)
+      setSelectedKey(keys[0])
+    } catch (error) {
+      LoggerInstance.error(error)
+    }
+  }
 
   async function handleSsiConnect() {
     try {
@@ -33,6 +67,8 @@ export default function SsiWalletManager() {
       setSsiWalletApiOverride(overrideApi)
       const session = await connectToWallet(signer!)
       setSessionToken(session)
+      const wallet = await fetchWallets(session)
+      await fetchKeys(wallet, session)
       setShowSsiWalletModule(false)
     } catch (error) {
       LoggerInstance.error(error)
