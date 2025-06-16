2c3f193ba102abd2a9094baa235ab46d3276a5ef
AdriGeorge <adrian7ko@gmail.com>
2025-06-16 15:26:41 +0300

    fix: owner

--- src/components/Asset/AssetActions/Download/index.tsx
+++ src/components/Asset/AssetActions/Download/index.tsx
@@ -473,6 +473,7 @@ export default function Download({
             <AssetAction asset={asset} />
           </div>
           {!isFullPriceLoading &&
+            !isOwner &&
             (appConfig.ssiEnabled ? (
               <>
                 {verifierSessionCache &&
