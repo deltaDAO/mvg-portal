edbca3999015b05cc83ebf17429ecec860344bd8
AdriGeorge <adrian7ko@gmail.com>
2025-07-02 14:22:11 +0300

    fix: fix

--- src/components/Asset/Edit/FormEditComputeService.tsx
+++ src/components/Asset/Edit/FormEditComputeService.tsx
@@ -48,7 +48,8 @@ export default function FormEditComputeService({
         serviceEndpoint,
         queryResult?.results || [],
         accountId,
-        publisherTrustedAlgorithms
+        publisherTrustedAlgorithms,
+        true
       )
       return algorithmSelectionList
     },
