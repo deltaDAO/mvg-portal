f8d17a90909a74374f22af651b65b6ae2688c95c
Kevin Rohlf <123676073+KevinRohlf@users.noreply.github.com>
2026-01-27 14:58:53 +0100

    feat: update onboarding step ready

--- src/components/@shared/Onboarding/Steps/Ready.tsx
+++ src/components/@shared/Onboarding/Steps/Ready.tsx
@@ -5,33 +5,37 @@ import SuccessConfetti from '@components/@shared/SuccessConfetti'
 import Button from '@components/@shared/atoms/Button'
 
 interface ReadyStep {
-  learnMore: {
+  buttons: {
     text: string
     link: string
     linkLabel: string
-  }
+  }[]
   title: string
   body: string
   image: string
 }
 
 export default function Ready(): ReactElement {
-  const { learnMore, title, body, image }: ReadyStep = content
+  const { buttons, title, body, image }: ReadyStep = content
 
   return (
     <div className={styles.container}>
       <div className={styles.topSection}>
         <SuccessConfetti success={body} className={styles.body} />
         <div className={styles.learnMore}>
-          <span>{learnMore.text}</span>
-          <Button
-            style="primary"
-            href={learnMore.link}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {learnMore.linkLabel}
-          </Button>
+          {buttons.map((button, index) => (
+            <div key={index}>
+              <span>{button.text}</span>
+              <Button
+                style="primary"
+                href={button.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {button.linkLabel}
+              </Button>
+            </div>
+          ))}
         </div>
       </div>
       <img src={image} className={styles.image} />
